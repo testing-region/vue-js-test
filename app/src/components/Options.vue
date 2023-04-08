814e5e8d417e1131c55e4a4e01389a484394d9ea
David Saah <dasorange.hope@gmail.com>
2023-04-08 22:17:57 +0000

    add method to add new item to shopping list

--- app/src/components/Options.vue
+++ app/src/components/Options.vue
@@ -3,19 +3,38 @@ export default {
     data() {
         return {
             header: "Shopping List App",
+            newItem: "",
             items: [
                 { id: 1, label: "Orange" },
                 { id: 2, label: "Mangoes" },
                 { id: 3, label: "Tomatoes" },
                 { id: 4, label: "Ice Cream" },
-            ]
+            ],
+            count: 0
         }
+    },
+
+    methods: {
+        addItem(item) {
+            this.items.push({
+                id: this.count,
+                label: item
+            })
+            this.count++
+            this.newItem = ""
+        }
+    },
+
+    mounted() {
+        this.count = this.items.length
     },
 }
 </script>
 
 <template>
     <h1>{{ header }}</h1>
+    <input type="text" v-model.lazy="newItem" placeholder="Enter an item" />
+    <button type="button" class="add-item" @click="addItem(newItem)">Add item</button>
     <ul class="shopping-list">
         <li v-for="item in items" :key="item.id">{{ item.label }}</li>
     </ul>
@@ -24,5 +43,10 @@ export default {
 <style scoped>
 .shopping-list {
     list-style-type: none;
+    padding: 0;
+}
+
+.add-item {
+    margin-left: 5px;
 }
 </style>
