2031d7f42563beba852a486a5dd47a283f1f8b6b
David Saah <dasorange.hope@gmail.com>
2023-04-08 18:57:52 +0000

    initial commit

---
+++ app/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+    <div>
+    </div>
+    <HelloWorld msg="Hello World" />
+</template>
+
+<style scoped></style>
---
+++ app/src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+defineProps({
+    msg: String,
+})
+
+</script>
+
+<template>
+    <h1>{{ msg }}</h1>
+</template>
+
+<style scoped></style>
