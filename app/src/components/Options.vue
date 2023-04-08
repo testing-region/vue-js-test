7bed3042c7554883a698002b4b172e3f8c114ed0
David Saah <dasorange.hope@gmail.com>
2023-04-08 20:48:17 +0000

    Create shopping list with Options API; test v-for

---
+++ app/src/components/Options.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    data() {
+        return {
+            header: "Shopping List App",
+            items: [
+                { id: 1, label: "Orange" },
+                { id: 2, label: "Mangoes" },
+                { id: 3, label: "Tomatoes" },
+                { id: 4, label: "Ice Cream" },
+            ]
+        }
+    },
+}
+</script>
+
+<template>
+    <h1>{{ header }}</h1>
+    <ul class="shopping-list">
+        <li v-for="item in items" :key="item.id">{{ item.label }}</li>
+    </ul>
+</template>
+
+<style scoped>
+.shopping-list {
+    list-style-type: none;
+}
+</style>
