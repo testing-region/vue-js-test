eb4aad7175cf5250e6a6160ccb1009a01bdb95e7
David Saah <dasorange.hope@gmail.com>
2023-04-14 23:08:24 +0000

    switch test to composition api

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,18 +1,14 @@
 <script setup>
 
-import Options from './components/Options.vue'
-// import Composition from './components/Composition.vue'
+// import Options from './components/Options.vue'
+import Composition from './components/Composition.vue'
 
 </script>
 
 <template>
-    <Options />
+    <!--    <Options /> -->
 
-    <!--
-        <br />
-        <h1>Composition API</h1>
-        <Composition /> 
-        -->
+    <Composition />
 </template>
 
 <style scoped></style>
